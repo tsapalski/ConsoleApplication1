7d630745f2d1ae4908ca4e11b8a171eeec1bda8c
Tomasz Sapalski <tsapalski@paddypower.com>
2011-08-02 16:23:59 +0100

    commit2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        //styartdr df dtgdf
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test console application 1");
+        }
+    }
+}
