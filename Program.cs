ecf5177541d4f5e6bb9abcf728221380f5ab4892
Tomasz Sapalski <tsapalski@paddypower.com>
2011-08-03 12:50:36 +0100

    First Working NUnit test

--- Program.cs
+++ Program.cs
@@ -5,10 +5,17 @@ using System.Text;
 
 namespace ConsoleApplication1
 {
+    public class Calculator
+    {
+        public int Add(int i, int j)
+        {
+            return i + j;
+        }
+
+    }
+
     class Program
     {
-       
-
         static void Main(string[] args)
         {
             Console.WriteLine("Test console application 1 - full cycle test 1");
